0cdb58c1b230c1c65072633dd058eebbb982c29d
Keijo Korte <git@kvak.net>
2022-02-12 13:31:49 +0200

    Fetching events from meetup.com

--- get_events.py
+++ get_events.py
@@ -1,11 +1,14 @@
 import requests
 import re
 import pytz
+import os
 from datetime import datetime
 from markdownify import markdownify
 
+EVENTS_DIR = "content/events/"
+
 def create_event_file(name, data):
-    with open('content/events/'+name, "w") as f:
+    with open(EVENTS_DIR + name, "w") as f:
         f.write(data)
     return
 
@@ -17,30 +20,42 @@ def is_dst(dt=None, timezone="UTC"):
     timezone_aware_date = timezone.localize(dt, is_dst=None)
     return timezone_aware_date.tzinfo._dst.seconds != 0
 
+def check_events():
+    path, dirs, files = next(os.walk(EVENTS_DIR))
+    count = len(files)
+    return count-1 # minus _index.md
+
 def main():
     url = "https://api.meetup.com/HelSec/events?scroll=future_or_past"
-    r = requests.get(url)
-    for event in r.json():
-        event_data = "---\n"
-        event_data += "title: '{}'\n".format(event['name'])
-        #date: 2021-04-29 18:00 
-        #date: 2021-10-25T14:12:56+03:00
-        event_date = datetime.strptime("{} {}:00".format(event['local_date'], event['local_time']), '%Y-%m-%d %H:%M:%S')
-        summertime = is_dst(event_date, "Europe/Helsinki")
-        if summertime:
-            timezone = "+0300"
-        else:
-            timezone = "+0200"
-        event_data += "date: {}{}\n".format(str(event_date).replace(" ", "T"),timezone)
-        if event['is_online_event'] == True:
-            event_data += "stream: 'https://twitch.tv/helsec'\n"
-        event_data += "link: '{}'\n".format(event['link'])
-        event_data += "---\n\n"
-        description = markdownify(event['description'])
-        event_data += description
-        name = "{}_{}.md".format(event['local_date'],re.sub(r"[^a-zA-Z0-9_]","",event['name'].replace(" ", "_")))
-        create_event_file(name, event_data)
-        #print(event_data)
+    try:
+        r = requests.get(url)
+    except requests.exceptions.RequestException as e:
+        raise SystemExit(e)
+    events_count = len(r.json())
+    print("Events on meetup.com: {} and on disk: {}".format(events_count, check_events()))
+    if events_count > check_events():
+        print("Fetching new events from meetup.com")
+        for event in r.json():
+            event_data = "---\n"
+            event_data += "title: '{}'\n".format(event['name'])
+            event_date = datetime.strptime("{} {}:00".format(event['local_date'], event['local_time']), '%Y-%m-%d %H:%M:%S')
+            summertime = is_dst(event_date, "Europe/Helsinki")
+            if summertime:
+                timezone = "+0300"
+            else:
+                timezone = "+0200"
+            event_data += "date: {}{}\n".format(str(event_date).replace(" ", "T"),timezone)
+            if event['is_online_event'] == True:
+                event_data += "stream: 'https://twitch.tv/helsec'\n"
+            event_data += "link: '{}'\n".format(event['link'])
+            event_data += "---\n\n"
+            description = markdownify(event['description'])
+            event_data += description
+            name = "{}_{}.md".format(event['local_date'], re.sub(r"[^a-zA-Z0-9_]","",event['name'].replace(" ", "_")))
+            create_event_file(name, event_data)
+            #print(event_data)
+    else:
+        print("No new events on meetup.com")
 
 
 if __name__ == "__main__":
